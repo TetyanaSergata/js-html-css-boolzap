35e82d563c3fffc17e0bce17c85770d75e604cf3
Tetyana <tsergata@gmail.com>
2020-01-28 17:16:05 +0100

    Risposta dall’interlocutore

--- js/script.js
+++ js/script.js
@@ -40,8 +40,8 @@ function sentMessage() {
 
     // Data
     var data = new Date();
-    var hour = data.getHours();
-    var minute = data.getMinutes();
+    var hour = addZero(data.getHours());
+    var minute = addZero(data.getMinutes());
     var time = hour + ':' + minute;
     newMessage.find('.message-time').text(time);
 
@@ -51,6 +51,14 @@ function sentMessage() {
     $('input.message-sent').val('');
   }
   console.log(message);
+}
+
+// Funzione per l'ora completa
+function addZero(number) {
+  if(number < 10) {
+    number += '0';
+  }
+  return number;
 }
 
 // Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
@@ -64,6 +72,7 @@ function sentAnswer() {
   var minute = data.getMinutes();
   var time = hour + ':' + minute;
   autoAnswer.find('.message-time').text(time);
+
   autoAnswer.addClass('received');
-  $('.message-box.sent').append(autoAnswer);
+  $('.conversation-middle.active').append(autoAnswer);
 }
