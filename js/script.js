6344a751cce724aa2463978449f519957266f731
Tetyana <tsergata@gmail.com>
2020-01-28 13:02:59 +0100

    Il messaggio dall'input concluso

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
-  $('.icon-box').click(function () {
+  $('.icon-sent').click(function () {
     sentMessage();
+  });
+
+  $('.message-sent').keypress(function (event) {
+    if(event.which == 13) {
+      sentMessage();
+    }
   });
 });
 
-$('.message-sent').keypress(function (event) {
-  if(event.which == 13) {
-    sentMessage();
-  }
-});
 
 
 
@@ -35,9 +36,18 @@ function sentMessage() {
   if (message.length != 0) {
     var newMessage = $('.template .message-box').clone();
     console.log(newMessage);
-    newMessage.find('.message-text').text(newMessage);
+    newMessage.find('.message-text').text(message);
+
     // Data
-    
+    var data = new Date();
+    var hour = data.getHours();
+    var minute = data.getMinutes();
+    var time = hour + ':' + minute;
+
+    newMessage.find('.message-time').text(time);
+    newMessage.addClass('sent');
+    $('.conversation-middle').append(newMessage);
+    $('input.message-sent').val('');
   }
   console.log(message);
 }
